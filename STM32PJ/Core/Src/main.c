7883e811f1de46932f0ce0a66816d5a0bf3b5421
Tran-Cao-Nguyen <nguyen.tran9103@hcmut.edu.vn>
2023-10-22 20:56:58 +0700

    update

--- STM32PJ/Core/Src/main.c
+++ STM32PJ/Core/Src/main.c
@@ -186,6 +186,15 @@ void update7SEG ( int index ){
 		break ;
 	}
 }
+
+
+void updateClockBuffer ()
+{
+	led_buffer[0] = hour / 10;
+	led_buffer[1] = hour % 10;
+	led_buffer[2] = minute / 10;
+	led_buffer[3] = minute % 10;
+}
 /* USER CODE END 0 */
 
 /**
