47b00c7a13052bcae10aedf655ce99a85cd97f80
Tran-Cao-Nguyen <nguyen.tran9103@hcmut.edu.vn>
2023-10-22 20:20:41 +0700

    update EX2

--- STM32PJ/Core/Src/main.c
+++ STM32PJ/Core/Src/main.c
@@ -199,72 +199,9 @@ int main(void)
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
 
-  setTimer1(50);
-  setTimer2(100);
-  setTimer3(50);
-  int status = 1;
+
   while (1)
   {
-	  if (timer1_flag == 1)
-	  {
-		  setTimer1(50);
-		  //TO DO
-		  HAL_GPIO_TogglePin(LED_RED_GPIO_Port, LED_RED_Pin);
-
-
-	  }
-	  if (timer2_flag == 1)
-	  {
-		  setTimer2(100);
-		  HAL_GPIO_TogglePin(DOT_GPIO_Port, DOT_Pin);
-	  }
-
-	  if (timer3_flag == 1)
-	  {
-		  setTimer3(50);
-		  switch (status)
-		  {
-		  	case 1:
-		  		status = 2;
-		  		HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, 0);
-		  		HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, 1);
-		  		HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, 1);
-		  		HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, 1);
-		  		display7SEG(1);
-		  		break;
-		  	case 2:
-		  		status = 3;
-		  		HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, 1);
-		  		HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, 0);
-		  		HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, 1);
-		  		HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, 1);
-		  		display7SEG(2);
-		  		break;
-		  	case 3:
-		  		status = 4;
-		  		HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, 1);
-		  		HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, 1);
-		  		HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, 0);
-		  		HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, 1);
-		  		display7SEG(3);
-		  		break;
-		  	case 4:
-		  		status = 1;
-		  		HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, 1);
-		  		HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, 1);
-		  		HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, 1);
-		  		HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, 0);
-		  		display7SEG(0);
-		  		break;
-		  	default:
-		  		break;
-
-		  }
-	  }
-
-
-
-
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -394,9 +331,62 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
+int counter1 = 100;
+int counter2 = 50;
+int status = 1;
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef * htim)
 {
-	timerRun();
+	counter1--;
+	counter2--;
+	if (counter1 <= 0)
+	{
+		counter1 = 100;
+		HAL_GPIO_TogglePin(DOT_GPIO_Port, DOT_Pin);
+	}
+	if (counter2 <= 0)
+	{
+		counter2 = 50;
+		HAL_GPIO_TogglePin(LED_RED_GPIO_Port, LED_RED_Pin);
+		switch (status)
+		{
+			case 1:
+				status = 2;
+				HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, 0);
+				HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, 1);
+				HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, 1);
+				HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, 1);
+				display7SEG(1);
+				break;
+			case 2:
+				status = 3;
+				HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, 1);
+				HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, 0);
+				HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, 1);
+				HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, 1);
+				display7SEG(2);
+				break;
+			case 3:
+				status = 4;
+				HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, 1);
+				HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, 1);
+				HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, 0);
+				HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, 1);
+				display7SEG(3);
+				break;
+			case 4:
+				status = 1;
+				HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, 1);
+				HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, 1);
+				HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, 1);
+				HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, 0);
+				display7SEG(0);
+				break;
+			default:
+				break;
+
+				  }
+
+	}
 }
 /* USER CODE END 4 */
 
