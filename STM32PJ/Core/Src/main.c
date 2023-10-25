f958a65be0dd7aec967fb5e7d3a57a3dad0afcef
Tran-Cao-Nguyen <nguyen.tran9103@hcmut.edu.vn>
2023-10-25 08:09:55 +0700

    update Ex4

--- STM32PJ/Core/Src/main.c
+++ STM32PJ/Core/Src/main.c
@@ -168,18 +168,34 @@ void update7SEG ( int index ){
 	switch ( index ){
 	case 0:
 		// Display the first 7 SEG with led_buffer [0]
+		HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, 0);
+		HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, 1);
+		HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, 1);
+		HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, 1);
 		display7SEG(led_buffer[0]);
 		break ;
 	case 1:
 		// Display the second 7 SEG with led_buffer [1]
+		HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, 1);
+		HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, 0);
+		HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, 1);
+		HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, 1);
 		display7SEG(led_buffer[1]);
 		break ;
 	case 2:
 		// Display the third 7 SEG with led_buffer [2]
+		HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, 1);
+		HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, 1);
+		HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, 0);
+		HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, 1);
 		display7SEG(led_buffer[2]);
 		break ;
 	case 3:
 		// Display the forth 7 SEG with led_buffer [3]
+		HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, 1);
+		HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, 1);
+		HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, 1);
+		HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, 0);
 		display7SEG(led_buffer[3]);
 		break ;
 	default :
@@ -359,7 +375,6 @@ static void MX_GPIO_Init(void)
 /* USER CODE BEGIN 4 */
 int counter1 = 100;
 int counter2 = 25;
-int status = 1;
 int index_led = 0;
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef * htim)
 {
@@ -374,38 +389,18 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef * htim)
 	{
 		counter2 = 25;
 		HAL_GPIO_TogglePin(LED_RED_GPIO_Port, LED_RED_Pin);
-		switch (status)
+		switch (index_led)
 		{
+			case 0:
+				update7SEG(index_led);
+				break;
 			case 1:
-				status = 2;
-				HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, 0);
-				HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, 1);
-				HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, 1);
-				HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, 1);
 				update7SEG(index_led);
 				break;
 			case 2:
-				status = 3;
-				HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, 1);
-				HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, 0);
-				HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, 1);
-				HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, 1);
 				update7SEG(index_led);
 				break;
 			case 3:
-				status = 4;
-				HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, 1);
-				HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, 1);
-				HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, 0);
-				HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, 1);
-				update7SEG(index_led);
-				break;
-			case 4:
-				status = 1;
-				HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, 1);
-				HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, 1);
-				HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, 1);
-				HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, 0);
 				update7SEG(index_led);
 				break;
 			default:
@@ -413,7 +408,7 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef * htim)
 
 		}
 		index_led++;
-		if (index_led >= 4) index_led = 0;
+		if (index_led >= MAX_LED) index_led = 0;
 
 	}
 }
